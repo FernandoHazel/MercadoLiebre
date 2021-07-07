11ac5db566cf7a02df5d5aaf4aca1d3774b27020
FernandoHazel <A01229544@itesm.mx>
2021-07-06 21:22:25 -0500

    added start

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get('/Login', (req, res)=>{
     res.sendFile(__dirname + '/Views/Login.html')
 })
 
-app.listen(process.env.PORT, ()=> { 
+app.listen(process.env.PORT || 3000, ()=> { 
     console.log('Servidor corriendo');
 })
